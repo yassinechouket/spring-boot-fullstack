132e2dc867d150c7e4fdcb7a9f3b529594fe8484
yassinechouket <medyassinechouket.2@gmail.com>
2025-09-24 23:01:29 +0100

    Initial commit - Spring Boot Fullstack project

---
+++ src/main/java/com/chouket370/springbootfullstack/SpringBootFullstackApplication.java
@@ -0,0 +1,13 @@
+package com.chouket370.springbootfullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFullstackApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/chouket370/springbootfullstack/student/Gender.java
@@ -0,0 +1,7 @@
+package com.chouket370.springbootfullstack.student;
+
+public enum Gender {
+    MALE,
+    FEMALE,
+    OTHER
+}
